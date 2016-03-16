88e39ac8da75f82a44e6168b3b0ad1b8f7d671be
Antinea Guerguichon <aguergui@ccage017.in2p3.fr>
2016-03-16 17:42:17 +0100

    structuration

--- Calibration_bias/Analysis.h
+++ Calibration_bias/Analysis.h
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <string>
+
 #include "TH1.h"
 
+#include <boost/multi_array.hpp>
 
-Class Analysis
+class Analysis
 {
+
 public:
   Analysis();
   ~Analysis();
   double GetMeanForConf(TH1D* h, unsigned int stat, double rms, double input);
   TH1D* FitGauss(TH1D* h, double xmin, double xmax);
+  void WriteFileStats (boost::multi_array <TH1D*, 2> h, boost::multi_array <unsigned int, 2> nEff, boost::multi_array <unsigned int, 2> stat, boost::multi_array <double, 2> input, unsigned int nbin, std::string outFileName);
+
 };
